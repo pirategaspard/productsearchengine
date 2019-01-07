79cc2565a2908f2de0bbda93c6b0616919294d6f
pirategaspard <dan@danielgaspar.com>
2019-01-06 20:35:56 -0500

    clean migration. fixed url

--- src/Migrations/Version20190107013255.php
+++ src/Migrations/Version20190107013255.php
@@ -8,15 +8,15 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190103044859 extends AbstractMigration
+final class Version20190107013255 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, source_id INT DEFAULT NULL, id_code VARCHAR(50) NOT NULL, price INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, url VARCHAR(255) NOT NULL, url_image VARCHAR(255) DEFAULT NULL, date_last_updated DATETIME DEFAULT NULL, data LONGTEXT NOT NULL, INDEX IDX_D34A04AD953C1C61 (source_id), FULLTEXT INDEX IDX_D34A04AD5E237E06ADF3F363 (name, data), UNIQUE INDEX UNIQ_D34A04ADFC352C9A (id_code), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE source (id INT AUTO_INCREMENT NOT NULL, id_code VARCHAR(50) NOT NULL, title VARCHAR(255) DEFAULT NULL, url VARCHAR(255) NOT NULL, date_last_updated DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, source_id INT DEFAULT NULL, id_code VARCHAR(50) NOT NULL, price INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, url VARCHAR(255) NOT NULL, url_image VARCHAR(255) DEFAULT NULL, date_last_updated DATETIME DEFAULT NULL, data LONGTEXT NOT NULL, url_canonical VARCHAR(255) DEFAULT NULL, INDEX IDX_D34A04AD953C1C61 (source_id), FULLTEXT INDEX IDX_D34A04AD5E237E06ADF3F363 (name, data), UNIQUE INDEX UNIQ_D34A04ADFC352C9A (id_code), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE source (id INT AUTO_INCREMENT NOT NULL, id_code VARCHAR(50) DEFAULT NULL, title VARCHAR(255) DEFAULT NULL, url VARCHAR(255) NOT NULL, date_last_updated DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD953C1C61 FOREIGN KEY (source_id) REFERENCES source (id)');
     }
 
